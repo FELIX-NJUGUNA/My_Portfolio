8b3774c5a98d0829f2264c6d070b2f0df48a3275
FELIX-NJUGUNA <felixnjuguna443@gmail.com>
2024-05-23 16:14:41 +0300

    This is My portfolio

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function openmenu(){
 function closemenu(){
     sidemu.style.right = "-200px";
 }
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxvDQ1eYgxM3DoRGi2iedfvAQ9RWMCiUqKegkQopjw/dev'
+const form = document.forms['submit-to-google-sheet']
+
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+
